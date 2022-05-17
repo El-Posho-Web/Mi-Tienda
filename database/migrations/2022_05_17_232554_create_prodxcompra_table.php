9e806625b28290795de82cb2a315a72a0e43332a
JJT <juanjo_tonelli@hotmail.com>
2022-05-17 20:30:25 -0300

    Migracion de Productos por compras

---
+++ database/migrations/2022_05_17_232554_create_prodxcompra_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prodxcompra', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_compra');
+            $table->foreign('id_compra')->references('id_compra')->on('compras');
+            $table->unsignedBigInteger('id_producto');
+            $table->foreign('id_producto')->references('id_producto')->on('productos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prodxcompra');
+    }
+};
