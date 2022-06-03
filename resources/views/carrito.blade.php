75d9a7265f1cee9fa11cf739cbd6b4b0010da9a8
JJT <juanjo_tonelli@hotmail.com>
2022-06-03 09:21:54 -0300

    Modal con formulario para tarjeta de credito

--- resources/views/carrito.blade.php
+++ resources/views/carrito.blade.php
@@ -102,7 +102,7 @@
               {{-- BOTON REALIZAR COMPRA --}}
 
               <div class="sumarioBtn">
-                <a href="/mi-tienda/carrito/confirmar" class="btn">Realizar compra</a>
+                <a class="btn" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Realizar compra</a>
               </div>
 
             </div>
@@ -114,7 +114,70 @@
 
           </div>
           
-      </div>      
+      </div>  
+      <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalToggleLabel">Ingresar datos de pago</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <!-- Aqui comienza el form -->
+                <br>
+
+                <div class="div-payment-left-body">
+                  <p>INGRESA LOS DATOS DE LA TARJETA </p>
+
+                  <div class="div-payment-inside-divisor">
+
+                      <div class="div-payment-inside-divisor-half">
+                          <label for="name" class="left-text">Nombre</label>
+                          <input id="name" name="nombre" type="text" class="form-control"
+                              required="">
+                      </div>
+                      <div class="div-payment-inside-divisor-half">
+                          <label for="lastName" class="left-text">Apellido</label>
+                          <input id="lastName" name="apellido" type="text" class="form-control"
+                              required="">
+                      </div>
+                      
+                  </div>
+
+                  <label for="cardnumber" class="left-text">Numero de tarjeta</label>
+                  <div class="div-payment-inside-divisor position-relative">
+                      
+                       <input id="cardnumber" name="tarjeta" type="text" class="form-control">
+                  </div>
+                  
+                 
+                  <div class="div-payment-inside-divisor">
+
+                      <div class="div-payment-inside-divisor-half">
+                          <label for="expirationdate" class="left-text">Fecha de vencimiento (MM/AA)</label>
+                          <input id="expirationdate" name="fechaVto" type="text" class="form-control">
+                      </div>
+                      <div class="div-payment-inside-divisor-half">
+                          <label for="securitycode" class="left-text">Codigo de seguridad</label>
+                          <input id="securitycode" name="codigo" type="text" class="form-control">
+                      </div>
+                      
+                  </div>
+
+
+                </div>
+                <div class="form-check">
+                  <input class="float-start flex-row w-auto mt-2" type="checkbox" value="1" name="chkTarjeta">
+                  <label class="m-lg-2 form-check-label" for="flexCheckDefault">
+                    ¿Desea almacenar los datos de su tarjeta?
+                  </label>
+                </div>
+                <a href="/mi-tienda/carrito/confirmar" class="btn float-end" >Confirmar</a>
+
+            </div>
+          </div>
+        </div>
+      </div>   
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
 </body>
