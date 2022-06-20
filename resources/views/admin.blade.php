e84511a6da3c71438c20bd160317eeb6f1372f1f
Joaquin Duran <duranexj99@gmail.com>
2022-06-20 16:42:28 -0300

    cartelitos de confirmación

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,13 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include('head')
     <link rel="stylesheet" href="/css/admin.css">
-    <!-- <link rel="stylesheet" href="/css/main.css"> -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <script src="https://kit.fontawesome.com/be85762cea.js" crossorigin="anonymous"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/footer.css">
     <title>Admin</title>
 </head>
 <body>
@@ -61,7 +57,7 @@
                             </div>
                         </div>
                     
-                    <form method="POST" action="/mi-tienda/admin/producto">
+                    <form method="POST" action="/mi-tienda/admin/producto" onsubmit="return confirm('Esta seguro que quiere agregar este producto?');">
                         @csrf
                     
 
@@ -105,7 +101,7 @@
                         </div>
                         
                         </div>
-                        </form>
+                    </form>
 
                     </div>
 
@@ -127,7 +123,7 @@
                     </div>
                     
 
-                <form method="POST" action="/mi-tienda/admin/categoria">
+                <form method="POST" action="/mi-tienda/admin/categoria" onsubmit="return confirm('Esta seguro que quiere agregar esta categoria?');">
                     @csrf
 
                     <div class="inputContainersRow mb-5">
@@ -167,15 +163,12 @@
                 <td>{{ $categoria->nombre }}</td>
                 <td>
                     <div class="divEdit">
-                    {{-- <a class="buttonAdm" href="/mi-tienda/admin/producto/{{ $producto->id_producto }}/editar" style="text-decoration:none; font-weight:300; width:100%; padding:5%; margin-top:10% !important">Modificar categoria</a> --}}
-     
                     </div>
                 </td>
-                <td><form method="POST" action="/mi-tienda/admin/categoria/{{ $categoria->id_categoria }}/eliminar">
+                <td><form  method="POST" action="/mi-tienda/admin/categoria/{{ $categoria->id_categoria }}/eliminar" onsubmit="return confirm('Esta seguro que quiere eliminar esta categoria?');">
                     @csrf 
                     @method('DELETE')
                     <button class="buttonAdm" style="font-weight:300; width:100%; padding:5%; margin-top:10%">Eliminar</button>
-                    
                 </form></td>
             </tr>
             @endforeach
@@ -220,15 +213,13 @@
                 <td>{{ $producto->precio_unitario }}</td>
                 <td>
                     <div class="divEdit">
-                    <a class="buttonAdm" href="/mi-tienda/admin/producto/{{ $producto->id_producto }}/editar" style="text-decoration:none; font-weight:300; width:100%; padding:5%; margin-top:10% !important">Modificar producto</a>
-
+                        <a class="buttonAdm" href="/mi-tienda/admin/producto/{{ $producto->id_producto }}/editar" style="text-decoration:none; font-weight:300; width:100%; padding:5%; margin-top:10% !important">Modificar producto</a>
                     </div>
                 </td>
-                <td><form method="POST" action="/mi-tienda/admin/producto/{{ $producto->id_producto }}/eliminar">
+                <td><form method="POST" action="/mi-tienda/admin/producto/{{ $producto->id_producto }}/eliminar" onsubmit="return confirm('Esta seguro que quiere eliminar este producto?');">
                     @csrf 
                     @method('DELETE')
                     <button class="buttonAdm" style="font-weight:300; width:100%; padding:5%; margin-top:10%">Eliminar</button>
-                    
                 </form></td>
             </tr>
             @endforeach
@@ -263,39 +254,37 @@
                 <tr><th>ID COMPRA</th><th>NOMBRE Y APELLIDO</th><th>DNI</th><th>DOMICILIO</th><th>ESTADO</th><th></th><th></th></tr>
 
                     @foreach ($envios as $envio)
-                    <tr>
-                    <td>{{ $envio->id_compra }}</td>
-                    <td>{{ $envio->compra->usuario->nombre . " " . $envio->compra->usuario->apellido }}</td>
-                    <td>{{ $envio->compra->usuario->dni }}</td>
-                    <td>{{ $envio->compra->usuario->domicilio }}</td>
-                    <td>{{ $envio->estado->nombre }}</td>
-                    @if ($envio->id_estado_envio == 1)
-                    <td>
-                        <form method="POST" action="/mi-tienda/admin/envio/{{ $envio->id_envio }}/editar">
-                            @csrf
-                            <input type="text" name="id_estado_envio" id="id_estado_envio" value="2" hidden>
-                            <button class="buttonAdm" style="font-weight:300; width:100%; padding:5%; margin-top:10%">Confirmar</button>
-                        </form>
-                    </td>
-                    <td>
-                        <form method="POST" action="/mi-tienda/admin/envio/{{ $envio->id_envio }}/editar">
-                            @csrf
-                            <input type="text" name="id_estado_envio" id="id_estado_envio" value="3" hidden>
-                            <button class="buttonAdm" style="font-weight:300; width:100%; padding:5%; margin-top:10%">Cancelar</button>
-                        </form>
-                    </td>
-                    @else
-                    <td></td>
-                    <td></td>
-                    @endif
-                </tr>
-                @endforeach
+                        <tr>
+                            <td>{{ $envio->id_compra }}</td>
+                            <td>{{ $envio->compra->usuario->nombre . " " . $envio->compra->usuario->apellido }}</td>
+                            <td>{{ $envio->compra->usuario->dni }}</td>
+                            <td>{{ $envio->compra->usuario->domicilio }}</td>
+                            <td>{{ $envio->estado->nombre }}</td>
+                            {{-- BOTONES APARECEN SOLO CUANDO EL ENVIO ESTA PENDIENTE --}}
+                            @if ($envio->id_estado_envio == 1)
+                                <td>
+                                    <form method="POST" action="/mi-tienda/admin/envio/{{ $envio->id_envio }}/editar" onsubmit="return confirm('Esta seguro que quiere confirmar este envio?');">
+                                        @csrf
+                                        <input type="text" name="id_estado_envio" id="id_estado_envio" value="2" hidden>
+                                        <button class="buttonAdm" style="font-weight:300; width:100%; padding:5%; margin-top:10%">Confirmar</button>
+                                    </form>
+                                </td>
+                                <td>
+                                    <form method="POST" action="/mi-tienda/admin/envio/{{ $envio->id_envio }}/editar" onsubmit="return confirm('Esta seguro que quiere cancelar este envio?');">
+                                        @csrf
+                                        <input type="text" name="id_estado_envio" id="id_estado_envio" value="3" hidden>
+                                        <button class="buttonAdm" style="font-weight:300; width:100%; padding:5%; margin-top:10%">Cancelar</button>
+                                    </form>
+                                </td>
+                            @else
+                                <td></td>
+                                <td></td>
+                            @endif
+                        </tr>
+                    @endforeach
             </tbody>
         </table>
     </div>
-
-
-
   </div>
 </div>
 
@@ -304,6 +293,5 @@
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-
 
 </html>
