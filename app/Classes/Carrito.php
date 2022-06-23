892a91922951156216f3ca985f27456e3d865828
JJT <juanjo_tonelli@hotmail.com>
2022-06-23 11:51:44 -0300

    Comentarios en la clase carrito

--- app/Classes/Carrito.php
+++ app/Classes/Carrito.php
@@ -6,13 +6,30 @@ use App\Models\Producto;
 use App\Models\Compra;
 use App\Models\Envio;
 
+/**
+ * Esta clase se encarga de toda la logica del carrito de compras.
+ *
+ *
+ * @author  Juan José Tonelli <tonelli.juanjo@gmail.com>
+ * @version 1.0
+ *
+ */
 class Carrito{
 
+    /**
+     * Devuelve un array con los items agregados al carrito. En el caso que no haya nada agregado se devuelve al arreglo vacio.
+     * 
+     * @return array
+     */
     public function get_carrito(){
         return session()->get('carrito', []);
     }
 
-
+    /**
+     * Devuelve el precio total de los items que hay en el carrito.
+     * 
+     * @return float
+     */
     public function get_total()
     {
         $carrito = CarritoFacade::get_carrito();
@@ -26,6 +43,12 @@ class Carrito{
         return $total;
     }
 
+    /**
+     * Agrega un producto al carrito guardandolo en la sesion del usuario, en el caso de que exista el producto ya en el carrito se devulve false y no es agregado.
+     * @param Producto $producto
+     * @param mixed $request
+     * @return bool
+     */
     public function agregar(Producto $producto, $request)
     {
         $carrito = Carrito::get_carrito();
@@ -48,6 +71,11 @@ class Carrito{
         return true;
     }
 
+    /**
+     * Devuelve si el producto con el id que se pasa como parametro existe en el carrito.
+     * @param integer $id_producto
+     * @return bool
+     */
     public function tiene($id_producto)
     {
         $carrito = Carrito::get_carrito();
@@ -60,6 +88,11 @@ class Carrito{
 
     }
 
+    /**
+     * Elimina un producto del carrito con el id del producto pasada en la $request. 
+     * Si no hay mas productos en el carrito luego de eliminar el elemento se olvida la clave en la sesion.
+     * @param mixed $request
+     */
     public function eliminar($request)
     {
         $carrito = Carrito::get_carrito();
@@ -77,6 +110,12 @@ class Carrito{
         }
     }
 
+    /**
+     * Confirma la compra. 
+     * En caso que no haya productos en el carrito, la compra sea de mas de 1000 o ya no haya mas stock al momento de confirmar la compra se devuelve un mensaje de error y se elimina el carrito de la sesion. 
+     * Si se confirma la compra se crean los registros en la BD y se elimina el carrito de la sesion.
+     * @return string|void
+     */
     public function confirmar()
     {
         $carrito = Carrito::get_carrito();
