7cece4446ab50b3c4d0ae49606ff2388fb4e1a02
Joaquin Duran <duranexj99@gmail.com>
2022-06-24 13:55:49 -0300

    docblock actualizado

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,21 +9,23 @@ use App\Models\User;
 use Illuminate\Validation\ValidationException;
 use App\Models\Envio;
 
-/* Esta clase se encarga de la logica de negocios de la pantalla del administrador */
-/* @author Joaquin Durán <duranexj99@gmail.com>
-@version 1.0 */
+/** Esta clase se encarga de la logica de negocios de la pantalla del administrador 
+* @author Joaquin Durán <duranexj99@gmail.com>
+* @version 1.0 */
 
 class AdminController extends Controller
 {
-    /* Devuelve la vista del admin 
-    @return view */
+    /**
+     * Devuelve la vista del admin 
+     *@return view */
     public function admin(){
         return view('admin');
     }
 
-    /* Crea un nuevo producto
-    Se valida los datos enviados en el formulario 
-    @return redirect */
+    /**
+     *  Crea un nuevo producto
+     *  Se valida los datos enviados en el formulario 
+     *  @return redirect */
 
     public function producto(){
         $attributes = request()->validate([
@@ -38,9 +40,9 @@ class AdminController extends Controller
 
         return redirect('mi-tienda/admin')->with('mensaje', 'Producto creado exitosamente.');
     }
-
-    /* Crea una nueva categoria. Valida los datos enviados en el formulario */
-    /* @return redirect */
+    /**
+     * Crea una nueva categoria. Valida los datos enviados en el formulario
+     * @return redirect */
 
     public function categoria(){
         $attributes = request()->validate([
@@ -51,17 +53,18 @@ class AdminController extends Controller
 
         return redirect('mi-tienda/admin')->with('mensaje', 'Categoria creada exitosamente.');
     }
-
-    /* Devuelve la vista para modificar el producto seleccionado.*/
-    /* @param $producto */
+    /**
+     * Devuelve la vista para modificar el producto seleccionado.
+     * @param Producto $producto 
+     * @return view */
 
     public function editarproducto(Producto $producto){
         return view('editarproducto', ['producto' => $producto]);
     }
 
-    /* Actualiza el producto seleccionado. Vuelve a la pantalla del admin. */
-    /* @param $producto */
-    /* @return redirect */
+    /** Actualiza el producto seleccionado. Vuelve a la pantalla del admin. 
+     * @param Producto $producto 
+     * @return redirect */
 
     public function actualizarproducto(Producto $producto){
         $attributes = request()->validate([
@@ -77,18 +80,18 @@ class AdminController extends Controller
         return redirect('mi-tienda/admin')->with('mensaje', 'Producto actualizado exitosamente.');
     }
 
-    /* Elimina el producto seleccionado 
-    @param $producto
-    @return redirect */
+    /** Elimina el producto seleccionado 
+     *@param Producto $producto
+     *@return redirect */
 
     public function destruirproducto(Producto $producto){
         $producto->delete();
         return redirect('mi-tienda/admin')->with('mensaje', 'Producto eliminado exitosamente.');
     }
 
-    /* Elimina la categoria seleccionada. Si existe algun producto en dicha categoria, esta no se puede eliminar.
-    @param $categoria
-    @return redirect */
+    /** Elimina la categoria seleccionada. Si existe algun producto en dicha categoria, esta no se puede eliminar.
+    *@param Categoria $categoria
+    *@return redirect */
 
     public function destruircategoria(Categoria $categoria){
         if (count($categoria->productos) > 0){
@@ -100,9 +103,9 @@ class AdminController extends Controller
 
     }
 
-    /* Modifica el estado del envio. Puede ser a confirmado o cancelado.
-    @param $envio
-    @return redirect */
+    /** Modifica el estado del envio. Puede ser a confirmado o cancelado.
+     *@param Envio $envio
+     *@return redirect */
 
     public function actualizarenvio(Envio $envio){
         $attributes = request()->validate([
